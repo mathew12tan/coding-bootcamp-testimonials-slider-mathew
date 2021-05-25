c853eff7194af717a06b98a2906755d63bea3989
Mathew <mathew12tan@gmail.com>
2021-05-25 15:01:52 +0800

    third commit

--- main.js
+++ main.js
@@ -1,57 +1,100 @@
-const avatars = document.querySelectorAll(".avatar");
-const testimonialWordings = document.querySelectorAll(".testimonial-wording");
-
-const preBtn = document.querySelector(".preBtn");
-const nextBtn = document.querySelector(".nextBtn");
-
-for (let index = 0; index < avatars.length; index++) {
-  avatars[0].classList.add("avatar-toggle-pre");
-  testimonialWordings[0].classList.add("testimonial-wording-toggle-pre");
-
-  if (avatars[index].classList.contains("avatar-toggle-pre")) {
-    let activeAvatar = avatars[index];
-    let activeWording = testimonialWordings[index];
-
-    nextBtn.addEventListener("click", function () {
-      activeAvatar.className = "avatar";
-      activeWording.className = "testimonial-wording";
-
-      if (activeAvatar.nextElementSibling === null) {
-        avatars[0].className = "avatar avatar-toggle-next";
-        activeAvatar = avatars[0];
-        
-        activeWording.className = 'testimonial-wording';
-        testimonialWordings[0].className = "testimonial-wording testimonial-wording-toggle-next";
-        activeWording = testimonialWordings[0];
-      } else {
-        activeAvatar.nextElementSibling.className = "avatar avatar-toggle-next";
-        activeAvatar = activeAvatar.nextElementSibling;
-
-        activeWording.className = 'testimonial-wording';
-        activeWording.nextElementSibling.className = "testimonial-wording testimonial-wording-toggle-next";
-        activeWording = activeWording.nextElementSibling;
-      }
-    });
-
-    preBtn.addEventListener("click", function () {
-      activeAvatar.className = "avatar";
-      activeWording.className = "testimonial-wording";
-
-      if (activeAvatar.previousElementSibling === null) {
-        avatars[avatars.length - 1].className = "avatar avatar-toggle-pre";
-        activeAvatar = avatars[avatars.length - 1];
-
-        activeWording.className = 'testimonial-wording';
-        testimonialWordings[testimonialWordings.length - 1].className = "testimonial-wording testimonial-wording-toggle-pre";
-        activeWording = testimonialWordings[testimonialWordings.length - 1];
-      } else {
-        activeAvatar.previousElementSibling.className = "avatar avatar-toggle-pre";
-        activeAvatar = activeAvatar.previousElementSibling;
-
-        activeWording.className = 'testimonial-wording';
-        activeWording.previousElementSibling.className = "testimonial-wording testimonial-wording-toggle-pre";
-        activeWording = activeWording.previousElementSibling;
-      }
-    });
+const testimonials = [
+  {
+    imgSrc: './images/image-tanya.jpg',
+    imgAlt: 'image-tanya',
+    intro: '" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m nowin the job of my dreams and so excited about the future. "',
+    person: 'Tanya Sinclair1',
+    title: 'UX Engineer'
+  },
+  {
+    imgSrc: './images/image-john.jpg',
+    imgAlt: 'image-john',
+    intro: '" If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into isincredible. I now feel so confident about starting up as a professional developer. "',
+    person: 'John Tarkpor2',
+    title: 'Junior Front-end Developer'
+  },
+  {
+    imgSrc: './images/image-tanya.jpg',
+    imgAlt: 'image-tanya',
+    intro: '" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m nowin the job of my dreams and so excited about the future. "',
+    person: 'Tanya Sinclair3',
+    title: 'UX Engineer'
+  },
+  {
+    imgSrc: './images/image-john.jpg',
+    imgAlt: 'image-john',
+    intro: '" If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into isincredible. I now feel so confident about starting up as a professional developer. "',
+    person: 'John Tarkpor4',
+    title: 'Junior Front-end Developer'
   }
+]
+
+const avatarGroup = document.querySelector('.avatar-group');
+const testimonialWordingGroup = document.querySelector('.testimonial-wording-group');
+
+testimonials.forEach(testimonial => {
+  const avatarImg = document.createElement('img');
+  avatarImg.className = 'avatar';
+  avatarImg.setAttribute('src', `${testimonial.imgSrc}`);
+  avatarImg.setAttribute('alt', `${testimonial.imgAlt}`);
+  avatarGroup.appendChild(avatarImg);
+
+  const wordingDiv = document.createElement('div');
+  wordingDiv.className = 'testimonial-wording';
+
+  const introH1 = document.createElement('h1');
+  introH1.className = 'intro';
+  introH1.textContent = testimonial.intro;
+
+  const personPara = document.createElement('p');
+  personPara.className = 'person';
+  personPara.textContent = testimonial.person;
+
+  const titlePara = document.createElement('p');
+  titlePara.className = 'title';
+  titlePara.textContent = testimonial.title;
+
+  wordingDiv.appendChild(introH1);
+  wordingDiv.appendChild(personPara);
+  wordingDiv.appendChild(titlePara);
+  testimonialWordingGroup.appendChild(wordingDiv);
+});
+
+
+const preBtn = document.querySelector('.preBtn');
+const nextBtn = document.querySelector('.nextBtn');
+
+const avatars = document.querySelectorAll('.avatar');
+const testimonialWordings = document.querySelectorAll('.testimonial-wording');
+
+let activeIndex = 0;
+avatars[activeIndex].classList.add('avatar-pre');
+testimonialWordings[activeIndex].classList.add('testimonial-wording-pre');
+
+function hideTestimonial() {
+  avatars[activeIndex].className = 'avatar';
+  testimonialWordings[activeIndex].className = 'testimonial-wording';
 }
+
+function showTestimonial(e) {
+  e.target.classList.contains('preBtn') ? (
+    avatars[activeIndex].className = 'avatar avatar-pre',
+    testimonialWordings[activeIndex].className = 'testimonial-wording testimonial-wording-pre'
+  ) : (
+    avatars[activeIndex].className = 'avatar avatar-next',
+    testimonialWordings[activeIndex].className = 'testimonial-wording testimonial-wording-next'
+  )
+}
+
+preBtn.addEventListener('click', function (e) {
+  hideTestimonial();
+  activeIndex === 0 ? activeIndex = (testimonials.length) - 1 : activeIndex--;
+  showTestimonial(e);
+})
+
+nextBtn.addEventListener('click', function (e) {
+  hideTestimonial()
+  activeIndex === testimonials.length - 1 ? activeIndex = 0 : activeIndex++;
+  showTestimonial(e);
+})
+
